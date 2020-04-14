942fcc9df1a314c79682cd44cfdf5ce0ed02c528
wayne-daniels <wa.daniels@hotmail.com>
2020-04-14 01:29:28 +0000

    Adjusted Feature on Catalog View

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function ProductListItem(props) {
   return (
-    <div id={props.productId} onClick={props.setViewDetail} style={{ cursor: 'pointer' }} className="d-flex flex-row align-items-center h-45 col-md-3 mb-4 mx-4 shadow bg-white">
-      <div className="inner-shadow">
+    <div id={props.productId} onClick={props.setViewDetail} style={{ cursor: 'pointer' }} className="d-flex flex-row h-45 col-md-3 mb-4 mx-4 shadow bg-white">
+      <div className="d-flex select vw-100 vh-100 align-items-center">
         <div className="d-flex flex-column justify-content-center card-body">
           <img src={props.product.image} className="card-img-top h-50" alt={props.product.name} />
           <h3 className="card-title mt-4">{props.product.name}</h3>
